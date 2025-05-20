84203c3137e7f129c9fd1c5db148a1c240eca5df
aymardieguez <salgadodieguezaymar@gmail.com>
2025-05-20 09:56:10 +0200

    codigo_sql_proyecto_aymar.sql

---
+++ sql/codigo_proyecto_aymar.sql
@@ -0,0 +1,36 @@
+use master 
+go
+create database proyectos_sostenibles_aymar
+go
+use proyectos_sostenibles_aymar
+go
+create table organizadores(
+nombre varchar(30)  not null primary key,
+contacto varchar(100)
+)
+create table tipo_evento(
+tipo varchar(100) not null primary key
+)
+create table eventos(
+nombre varchar(30) not null primary key,
+fecha date not null,
+duracion smallint not null,
+lugar varchar(100) not null,
+tipo varchar(100) not null references tipo_evento(tipo),
+organizador varchar(30) not null references organizadores(nombre)
+	on update no action
+	on delete cascade
+)
+create table usuarios(
+correo_electronico varchar(200) not null primary key,
+nombre varchar(30)not null,
+contraseña varchar(30) not null
+)
+create table registran(
+evento varchar(30) not null references eventos(nombre),
+usuario varchar(200) not null references usuarios(correo_electronico)
+	on update no action
+	on delete cascade,
+asistentes smallint not null,
+primary key(evento, usuario)
+)
